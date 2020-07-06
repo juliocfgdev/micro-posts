680cbd3f9b2e7c8daaba879c181bd04a59899322
Julio Cesar <juliocefg@gmail.com>
2020-07-06 14:18:59 -0300

    Add posts and alerts

--- src/app.js
+++ src/app.js
@@ -4,8 +4,31 @@ import { ui } from './ui'
 // Get Posts on DOM load
 document.addEventListener('DOMContentLoaded', getPosts);
 
+// Listener for submit post
+document.querySelector('.post-submit').addEventListener('click', submitPost)
+
+// Get Posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
     .then(data => ui.showPosts(data))
     .catch(err => console.log(err))
+}
+
+function submitPost() {
+  const title = document.querySelector('#title').value
+  const body = document.querySelector('#body').value
+
+  const data = {
+    title,
+    body
+  }
+
+  // Create Post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post Added', 'alert alert-success text-center')
+      ui.clearFields()
+      getPosts()
+
+    }).catch((err) => console.log(err));
 }
--- src/ui.js
+++ src/ui.js
@@ -2,7 +2,7 @@ class UI {
     constructor() {
         this.post = document.querySelector('#posts')
         this.titleInput = document.querySelector('#title')
-        this.titleBody = document.querySelector('#body')
+        this.bodyInput = document.querySelector('#body')
         this.idInput = document.querySelector('#id')
         this.postSubmit = document.querySelector('.post-submit')
         this.forState = 'add'
@@ -26,7 +26,43 @@ class UI {
 
         this.post.innerHTML = output
     }
+    showAlert(message, className) {
+        this.clearAlert()
+
+        // Create div
+        const div = document.createElement('div')
+        // Add classes
+        div.className = className;
+        // Add text
+        div.appendChild(document.createTextNode(message))
+        // Get parent
+        const container = document.querySelector('.postsContainer')
+        // Get posts
+        const posts = document.querySelector('#posts')
+        // Insert alert div
+        container.insertBefore(div, posts)
+
+        // Timeout
+        setTimeout(() => {
+            this.clearAlert();
+        }, 3000);
+    }
+
+    clearAlert() {
+        const currentAlert = document.querySelector('.alert')
+        if (currentAlert) {
+            currentAlert.remove();
+        }
+    }
+
+    clearFields() {
+        this.titleInput.value = '';
+        this.bodyInput.value = '';
+    }
+
 
 }
+
+
 
 export const ui = new UI();
